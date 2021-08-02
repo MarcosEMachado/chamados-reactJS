d25951edcf2c4742eeb47dd03ba86a4d8312f648
MarcosEMachado <68864709+MarcosEMachado@users.noreply.github.com>
2021-08-02 14:55:52 -0300

    aula 88

--- src/paginas/Dashboard/index.js
+++ src/paginas/Dashboard/index.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
 
 export default function Dashboard(){
+
+    const { user } = useContext(AuthContext); 
     return(
         <div>
-            <h1>PAGINA Dashboard</h1>
+            <h1>PAGINA Dashboard user: {user.nome}</h1>
         </div>
     )
 }
