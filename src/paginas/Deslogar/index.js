392a4d94b0912b5b27000c341d886c1f969b1196
MarcosEMachado <68864709+MarcosEMachado@users.noreply.github.com>
2021-08-02 14:00:18 -0300

    cadastro

--- src/paginas/Deslogar/index.js
+++ src/paginas/Deslogar/index.js
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../../assets/logo.png';
 
 function Deslogar() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('clicou')
+  }
+
     return (
-      <div>
-        <h1> pagina de cadastro </h1>
+      <div className= "container-center">
+        <div className="login">
+          <div className="logo-area">
+            <img src={logo} alt="Sistema Logo"/>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <h1>Cadastra</h1>
+            <input type="text" placeholder="Seu nome" value={nome} onChange={ (e)=> setNome(e.target.value) } />
+            <input type="text" placeholder="email@email.com" value={email} onChange={ (e)=> setEmail(e.target.value) } />
+            <input type="password" placeholder="******" value={senha} onChange={ (e)=> setSenha(e.target.value) }/>
+            <button type="submit">Acessar</button>
+          </form>
+          <Link to="/"> Ja tenho uma conta </Link>
+        </div>
       </div>
     );
   }
