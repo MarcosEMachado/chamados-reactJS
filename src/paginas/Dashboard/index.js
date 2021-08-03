dbfa57aba3d519a0e7b0265ae9f9a2937716e4e8
MarcosEMachado <68864709+MarcosEMachado@users.noreply.github.com>
2021-08-03 15:00:05 -0300

    101

--- src/paginas/Dashboard/index.js
+++ src/paginas/Dashboard/index.js
@@ -1,16 +1,70 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
+import { useState } from 'react';
+import './dashboard.css';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiMessageSquare , FiPlus, FiEdit2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard(){
 
-    const { user } = useContext(AuthContext); 
+    const [chamados, setChamados] = useState([1]);
 
     return(
         <div>
             <Header/>
             <div className="content">
-                <h1>PAGINA Dashboard user: {user.nome}</h1>
+                <Title name="Chamados">
+                    <FiMessageSquare size={25}/>
+                </Title>
+
+                {chamados.length === 0 ? (
+                    <div className="container dashboard">
+                        <span>Nenhum chamado registrado...</span>
+                        <Link to="/new" className="new" >
+                            <FiPlus size={25}/>
+                            Novo Chamado
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/new" className="new" >
+                            <FiPlus size={25}/>
+                            Novo Chamado
+                        </Link>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Numero</th>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Cadastrado em</th>
+                                    <th scope="col">#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td data-label="Numero">1</td>
+                                    <td data-label="Cliente">Sujeito</td>
+                                    <td data-label="Assunto">Suporte</td>
+                                    <td data-label="Status">
+                                        <span className="badge" style={{background: '#5cb85c'}}>Em aberto</span>
+                                    </td>
+                                    <td data-label="DtCadastro">20/06/2021</td>
+                                    <td data-label="#">
+                                        <button className="action" style={{background: '#3583f6'}}>
+                                            <FiMessageSquare color="#FFF" size={17}/>
+                                        </button>
+                                        <button className="action" style={{background: '#F6a935'}}>
+                                            <FiEdit2 color="#FFF" size={17}/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}
+                
             </div>
         </div>
     )
